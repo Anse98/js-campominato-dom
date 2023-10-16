3bec10ac72ea6ce339c755ac4ec7765f9d664d4d
Anse98 <lorenzo.anselmi98.com>
2023-10-16 18:56:30 +0200

    Codice js Ottimizzato

--- app.js
+++ app.js
@@ -8,95 +8,44 @@
 function generateGrill() {
 
   // Sostituisco il contenuto del button play
-  playBtnElement.innerHTML = `Nuova partita <i class="fa-solid fa-rotate-right"></i>` ;
+  playBtnElement.innerHTML = `Nuova partita <i class="fa-solid fa-rotate-right"></i>`;
 
   // Cancello la griglia ogni volta che viene premuto il tasto gioca
   gridElement.innerHTML = "";
 
-  // Mi vado a prendere la select dal DOM
-  const selectElement = document.getElementById("difficulty");
-
-  if (selectElement.value === "f") {
+  if (selectElement.value === "hard") {
 
     // Mi genera 49 caselle se la select è impostata su facile
-    generateGrillEasy();
+    generateGrillCell(49, 'hard');
 
-  } else if (selectElement.value === "m") {
+  } else if (selectElement.value === "medium") {
     {
       // Mi genera 81 caselle se la select è impostata su medio
-      generateGrillMedium();
+      generateGrillCell(81, 'medium');
     }
   } else {
     // Mi genera 100 caselle se la select è impostata su difficile
-    generateGrillHard ();
+    generateGrillCell(100, 'easy');
   }
 }
 
-// FUNZIONE CHE MI GENERA 49 CASELLE
+// FUNZIONE CHE MI GENERA LE CASELLE
 
-function generateGrillEasy () {
-  // Genero 49 volte le caselle
-  for (let i = 0; i < 100; i++) {
+function generateGrillCell(num, difficulty) {
+  // Genero tot volte le caselle
+  for (let i = 0; i < num; i++) {
     const n = i + 1;
 
     //Creo la cella che dovrò stampare tot volte nel DOM
-    const cellString = `<div class="cell cell-easy">${n}</div>`;
+    const cellString = `<div class="cell cell-${difficulty}">${n}</div>`;
 
     //Concateno ad ogni giro del ciclo la variabile cell
     gridElement.innerHTML += cellString;
   }
 }
 
-// FUNZIONE CHE MI GENERA 81 CASELLE
 
-function generateGrillMedium () {
-  // Genero 81 volte le caselle
-  for (let i = 0; i < 81; i++) {
-    const n = i + 1;
-
-    //Creo la cella che dovrò stampare tot volte nel DOM
-    const cellString = `<div class="cell cell-medium">${n}</div>`;
-
-    //Concateno ad ogni giro del ciclo la variabile cell
-    gridElement.innerHTML += cellString;
-  }
-}
-
-// FUNZIONE CHE MI GENERA 100 CASELLE
-
-function generateGrillHard () {
-  // Genero 100 volte le caselle
-  for (let i = 0; i < 49; i++) {
-    const n = i + 1;
-
-    //Creo la cella che dovrò stampare tot volte nel DOM
-    const cellString = `<div class="cell cell-difficult">${n}</div>`;
-
-    //Concateno ad ogni giro del ciclo la variabile cell
-    gridElement.innerHTML += cellString;
-  }
-}
-
-// FUNZIONE PER DARE BG COLOR ALLE CELLE E CONSOLE LOG DEL NUMERO DELLE CELLE
-
-function addBgAndN () {
-
-  // Vado a prendermi tutti gli elementi dal DOM con classe cell
-  const cellElements = document.querySelectorAll(".cell");
-
-  for (let i = 0; i < cellElements.length; i++) {
-    const n = i + 1;
-    const actualCell = cellElements[i];
-
-    // Aggiungo l'evento click ad ogni casella
-    actualCell.addEventListener("click", function () {
-      this.classList.add("bg-azure");
-      console.log(n);
-    })
-  }
-}
-
-// FUNZIONE CHE MI GENERA UN NUMERO CASUALE DA 1 A 100
+// FUNZIONE CHE MI GENERA UN NUMERO CASUALE
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -107,39 +56,59 @@ function getRandomInt(min, max) {
 // FUNZIONE CHE MI GENERA LE 16 BOMBE
 
 function generateBombs() {
+
   // Genero un array che conterrà le mie 16 bombe
-const bombs = [];
+  //const bombs = [];
+  bombs = [];
 
-// Inserisco nell'array tutti i 16 numeri diversi uno dall'altro
-while (bombs.length < 16 === true) {
+  // Inserisco nell'array tutti i 16 numeri diversi uno dall'altro
+  while (bombs.length < 16) {
 
-  n = getRandomInt(1,100);
+    let nBomb;
+    // In base al livello di difficoltà i numeri delle bombe generate variano
+    if (selectElement.value === "f") {
+      nBomb = getRandomInt(1, 100);
+    } else if (selectElement.value === "m") {
+      nBomb = getRandomInt(1, 81);
+    } else {
+      nBomb = getRandomInt(1, 49);
+    }
 
-  if (bombs.includes(n) === false) {
-    bombs.push(n);
+    // Se il numero non è presente nell'array allora inseriscilo
+    if (bombs.includes(nBomb) === false) {
+      bombs.push(nBomb);
+    }
   }
+  console.log(bombs);
+
 }
-console.log(bombs);
-}
 
-  /////////////////////////////FINE FUNZIONI////////////////////////////////////
+/////////////////////////////FINE FUNZIONI////////////////////////////////////
 
 
-  // Mi vado a prendere l'elemento del DOM che contiene le celle
-  const gridElement = document.querySelector(".grid");
+// Mi vado a prendere l'elemento del DOM che contiene le celle
+const gridElement = document.querySelector(".grid");
 
-  // Mi vado a prendere il button play dal DOM
-  const playBtnElement = document.getElementById("play-btn");
+// Mi vado a prendere il button play dal DOM
+const playBtnElement = document.getElementById("play-btn");
 
-  // Creiamo un evento al click del button
-  playBtnElement.addEventListener("click", function () {
+// Mi vado a prendere la select dal DOM
+const selectElement = document.getElementById("difficulty");
 
-    generateGrill();
+let bombs = [];
 
-    addBgAndN ();
+// Creiamo un evento al click del button
+playBtnElement.addEventListener("click", function () {
 
-    generateBombs();
-    
-  })
+  generateGrill();
 
-  
+  generateBombs();
+
+});
+
+// Creo un evento al click della griglia (e caselle)
+gridElement.addEventListener('click', function (event) {
+  const target = event.target;
+  const targetNumber = parseInt(target.innerHTML);
+  console.log(targetNumber);
+});
